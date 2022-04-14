b9d6bb73cced759f27747374e3e0e08aa1bcf293
CarinaKuhn <carinakuhnsouza@gmail.com>
2022-04-14 18:38:25 -0300

    Criação de botões e "linkagem" p/ o carrossel.

---
+++ js/owl/setup.js
@@ -0,0 +1,88 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
+$("#carrosel-item").hover(
+    function () {
+        $("#carrosel-buttons").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons").fadeOut("fast");
+
+    });
+
+$("#carrosel-item2").hover(
+    function () {
+        $("#carrosel-buttons2").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons2").fadeOut("fast");
+
+});
+
+$("#carrosel-item3").hover(
+    function () {
+        $("#carrosel-buttons3").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons3").fadeOut("fast");
+
+});
+
+$("#carrosel-item4").hover(
+    function () {
+        $("#carrosel-buttons4").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons4").fadeOut("fast");
+
+});
+
+$("#carrosel-item5").hover(
+    function () {
+        $("#carrosel-buttons5").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons5").fadeOut("fast");
+
+});
+
+$("#carrosel-item6").hover(
+    function () {
+        $("#carrosel-buttons6").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons6").fadeOut("fast");
+
+});
+
+$("#carrosel-item7").hover(
+    function () {
+        $("#carrosel-buttons7").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons7").fadeOut("fast");
+
+});
+
+$("#carrosel-item8").hover(
+    function () {
+        $("#carrosel-buttons8").fadeIn("fast");
+    },
+    function () {
+        $("#carrosel-buttons8").fadeOut("fast");
+
+});
